2890e6ec1916d1c5157a73c38d59ce1482546f2b
MohamedElsayed1 <momaroof61@gmail.com>
2024-09-05 12:49:51 +0300

    Task

--- C Programming/6. Pointers/Ex4_Print_Reverse_Array_Using_Pointer.c
+++ C Programming/6. Pointers/Ex4_Print_Reverse_Array_Using_Pointer.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
-#include <string.h>
 
+int countMaxOnesBetweenZeros(int num) {
+    int maxCount = 0;
+    int currentCount = 0;
+    int inSequence = 0; // Flag to indicate if we're between zeros
 
-int main()
-{
-
-    int arr[100];
-    int size;
-
-    printf(" Input 5 number of elements in the array : ");
-    scanf("%d", &size);
-
-    for(int i = 0; i < size; i++){
-        printf("element - %d :", i+1);
-        scanf("%d", arr+i);
+    while (num > 0) {
+        if (num & 1) { // Check if the least significant bit is 1
+            if (inSequence) {
+                currentCount++;
+            } else {
+                currentCount = 1;
+                inSequence = 1;
+            }
+        } else { // The bit is 0
+            if (inSequence) {
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+                currentCount = 0;
+                inSequence = 0;
+            }
+        }
+        num >>= 1; // Right shift to process the next bit
     }
+    
+    return maxCount;
+}
 
-    int *ptr = arr+size-1;
+int main() {
+    int num;
 
-    printf("\n The elements of array in reverse order are : \n");
+    // Test Case 1
+    num = 14; // Binary: 01110
+    printf("Max number of ones between zeros for %d: %d\n", num, countMaxOnesBetweenZeros(num)); // Output: 3
 
-    for(int i = size; i > 0; i--){
-        printf("element - %d : %d\n", i, *ptr--);
-    }
-    printf("\n");
+    // Test Case 2
+    num = 110; // Binary: 01101110
+    printf("Max number of ones between zeros for %d: %d\n", num, countMaxOnesBetweenZeros(num)); // Output: 3
 
     return 0;
 }
